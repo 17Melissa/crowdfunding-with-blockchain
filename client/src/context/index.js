84a04e85460a23462ff0ab70233229ad14309b64
Melissa <>
2023-11-16 12:13:23 -0500

    add navbar

---
+++ client/src/context/index.js
@@ -0,0 +1,1 @@
+export {default as useStateContext} from './useStateContext';
---
+++ client/src/context/useStateContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const useStateContext = () => {
+  return (
+    <div>useStateContext</div>
+  )
+}
+
+export default useStateContext
